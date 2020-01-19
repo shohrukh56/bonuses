ec8b106f6f3bf42c54df570861dea043b8521c32
shohrukh <shohrukh5665@gmail.com>
2020-01-19 10:53:31 +0500

    fixed:#2

--- main.go
+++ main.go
@@ -5,13 +5,11 @@ func main()  {
 func bonuses(sales []int) int {
 	const salesborder =10_000
 	const percent=5
-	totalbonuses:=0
-	bonus:=0
+	totalBonuses:=0
 	for _, sale:=range sales{
 		if sale > salesborder{
-			bonus=(sale-salesborder)*percent/100
-			totalbonuses+=bonus
+			totalBonuses += (sale-salesborder)*percent/100
 		}
 	}
-	return totalbonuses
+	return totalBonuses
 }
